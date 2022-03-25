bd5ee1f4e144efe787c1dc83e6a69c6fb5df4173
Konstantin Berezko <berezkok72@gmail.com>
2022-03-25 12:32:49 +0500

    fix(router6): fix match to route

--- src/components/CustomLink/CustomLink.js
+++ src/components/CustomLink/CustomLink.js
@@ -1,7 +1,12 @@
 import { Link, useMatch } from 'react-router-dom';
 
 const CustomLink = ({ children, to, ...otherProps }) => {
-  const match = useMatch(to);
+  // end: false - проверка по части to
+  // to.length === 1 у главной страницы '/'
+  const match = useMatch({
+    path: to,
+    end: to.length === 1,
+  });
 
   return (
     <Link
